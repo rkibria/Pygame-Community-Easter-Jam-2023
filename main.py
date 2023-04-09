41d9c6552e225222d515f7d07f1e5c86c5aec03f
Raihan Kibria <4503850+rkibria@users.noreply.github.com>
2023-04-09 11:59:50 +0100

    particles

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Pygame-Community-Easter-Jam-2023 entry
-https://itch.io/jam/pygame-community-easter-jam-2023
+https://itch.io/jam/pg-community-easter-jam-2023
 """
 
 # import asyncio # PYGBAG
@@ -8,7 +8,9 @@ https://itch.io/jam/pygame-community-easter-jam-2023
 import time
 import math
 
-import pygame
+import pygame as pg
+from pygame import Vector2
+from pygame import Color
 
 # Palette:
 # purple B2A4FF
@@ -21,42 +23,53 @@ class SpriteSheet(object):
     def __init__(self, file_name):
         # You have to call `convert_alpha`, so that the background of
         # the surface is transparent.
-        self.sprite_sheet = pygame.image.load(file_name).convert_alpha()
+        self.sprite_sheet = pg.image.load(file_name).convert_alpha()
 
     def get_image(self, x, y, width, height):
-        # Use a transparent surface as the base image (pass pygame.SRCALPHA).
-        image = pygame.Surface([width, height], pygame.SRCALPHA)
+        # Use a transparent surface as the base image (pass pg.SRCALPHA).
+        image = pg.Surface([width, height], pg.SRCALPHA)
         image.blit(self.sprite_sheet, (0,0), (x, y, width, height))
         return image
 
+def create_particle():
+    # 0 enable, 1 position, 2 mass, 3 velocity, 4 color
+    return [False, Vector2(), 1.0, Vector2(), Color(255, 255, 255)]
+
+def draw_particles(surface, particles):
+    pass
+
 def main_function(): # PYGBAG: decorate with 'async'
     """Main"""
-    pygame.init()
+    pg.init()
 
-    screen = pygame.display.set_mode((640, 480), flags=pygame.SCALED)
-    pygame.display.set_caption("Pastel Particle Overdose")
-    clock = pygame.time.Clock()
+    screen = pg.display.set_mode((640, 480), flags=pg.SCALED)
+    pg.display.set_caption("Pastel Particle Overdose")
+    clock = pg.time.Clock()
 
-    font = pygame.font.Font(None, 30)
+    font = pg.font.Font(None, 30)
     TEXT_COLOR = (200, 200, 230)
+
+    particles = []
+    for i in range(10):
+        particles.append(create_particle())
 
     frame = 0
     done = False
 
     while not done:
         clock.tick(30)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
                 done = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
                     done = True
-            elif event.type == pygame.KEYUP:
+            elif event.type == pg.KEYUP:
                 pass
 
         screen.fill((0, 0, 0))
 
-        pygame.display.flip()
+        pg.display.flip()
         frame += 1
         # await asyncio.sleep(0) # PYGBAG
 
